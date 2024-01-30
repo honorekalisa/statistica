9708cd68503c747bf704a699f8954d150a379fba
honorekalisa <h.kalisa@alustudent.com>
2024-01-30 11:11:20 +0200

    add key stats cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import KeyFigures from "@/components/ui/key-figures";
+
 export default function Home() {
-  return <div className="flex flex-col py-4 px-8 bg-slate-50 min-h-screen">hello world</div>;
+  return (
+    <div className="flex flex-col gap-2 py-2 px-8 bg-slate-50 min-h-screen">
+      <p className="text-sm">Key Figures</p>
+      <KeyFigures />
+    </div>
+  );
 }
